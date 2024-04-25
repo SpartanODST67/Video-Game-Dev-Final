b813c7f6d26e89cd2532b8c79681cd2dd2c7ebc8
Hans Landgrebe <hlxbl20@gmail.com>
2024-04-25 18:29:30 -0500

    Pause Menu now openable

--- Man's Oldest Game/Assets/Scripts/InputHandler.cs
+++ Man's Oldest Game/Assets/Scripts/InputHandler.cs
@@ -6,10 +6,36 @@ public class InputHandler : MonoBehaviour
 {
     [Header("Current Character")]
     [SerializeField] ControllableCharacter controlledCharacter;
+    [Header("Pause Menu")]
+    [SerializeField] GameObject pauseMenu;
+    public bool isGamePaused = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isGamePaused)
+            {
+                Time.timeScale = 1;
+                isGamePaused = false;
+                pauseMenu.SetActive(false);
+            }
+            else
+            {
+                Time.timeScale = 0;
+                isGamePaused = true;
+                pauseMenu.SetActive(true);
+            }
+        }
+    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (isGamePaused)
+        {
+            return;
+        }
         float verticalInput = Input.GetAxisRaw("Vertical");
         float horizontalInput = Input.GetAxisRaw("Horizontal");
         float extraInput = 0; //I'm sure I'm gonna need a 3rd input.
