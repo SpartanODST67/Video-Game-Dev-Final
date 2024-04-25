1fc0a6e7d3a0318b237424d4ca390bbaa8e5013e
Hans Landgrebe <hlxbl20@gmail.com>
2024-04-25 17:15:34 -0500

    Added new tiles and started pause menu

---
+++ Man's Oldest Game/Assets/Scripts/Inventory.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : ScriptableObject
+{
+    public List<Item> itemQuantities = new List<Item>();
+}
