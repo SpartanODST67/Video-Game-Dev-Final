342c4d6a4b8c9a3abfb5806f30fca55dcd5d3bf2
Hans Landgrebe <hlxbl20@gmail.com>
2024-04-26 12:00:04 -0500

    Save System Created

--- Man's Oldest Game/Assets/Scripts/Items/Inventory.cs
+++ Man's Oldest Game/Assets/Scripts/Items/Inventory.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "ScriptableObjects/Inventory")]
@@ -21,5 +23,53 @@ public class Inventory : ScriptableObject
     public void InitializeItemQuantities()
     {
         itemQuantities = new List<int>(itemDictionary.Count);
+    }
+
+    public void InitializeInventory()
+    {
+        for(int i = 0; i < itemDictionary.Count; i++)
+        {
+            try
+            {
+                itemQuantities[i] = 0;
+            } catch
+            {
+                itemQuantities.Add(0);
+            }
+        }
+    }
+
+    public string ToCSV()
+    {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < itemQuantities.Count; i++)
+        {
+            sb.Append(itemQuantities[i].ToString());
+            sb.Append(",");
+        }
+        sb.Length--;
+        return sb.ToString();
+    }
+
+    public void FromCSV(string line)
+    {
+        string[] entries = line.Split(",");
+        for(int i = 0; i < entries.Length; i++)
+        {
+            try
+            {
+                itemQuantities[i] = int.Parse(entries[i]);
+            } catch(Exception e)
+            {
+                Debug.LogException(e);
+                try
+                {
+                    itemQuantities.Add(int.Parse(entries[i]));
+                } catch(Exception f)
+                {
+                    Debug.LogException(f);
+                }
+            }
+        }
     }
 }
---
+++ Man's Oldest Game/Assets/Scripts/SaveSystem.cs
@@ -0,0 +1,118 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+
+public class SaveSystem : MonoBehaviour
+{
+    [Header("Saved Items")]
+    [SerializeField] string sceneName;
+    [SerializeField] Inventory playerInventory;
+
+    [Header("Save Location")]
+    [SerializeField] string fileName;
+    [SerializeField] string folderPath = "./Saves/";
+    public static SaveSystem instance { get; private set; }
+
+    private void Awake()
+    {
+        Singleton();
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Singleton()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+    }
+
+    public void SetSceneName(string sceneName)
+    {
+        this.sceneName = sceneName;
+    }
+
+    public void SetFileName(string fileName)
+    {
+        this.fileName = fileName;
+    }
+
+    public void SaveGame()
+    {
+        StringBuilder path = new StringBuilder(folderPath);
+        path.Append(fileName);
+
+        string filePath = path.ToString();
+        string inventorySave = playerInventory.ToCSV();
+
+        StringBuilder saveData = new StringBuilder(sceneName);
+        saveData.Append("\n");
+        saveData.Append(inventorySave);
+
+        if(!Directory.Exists(folderPath))
+        {
+            Directory.CreateDirectory(folderPath);
+        }
+
+        try
+        {
+            using (StreamWriter sw = File.CreateText(filePath))
+            {
+                sw.WriteLine(saveData.ToString());
+            }
+        } catch (Exception e)
+        {
+            throw e;
+        }
+    }
+
+    //Returns 0 on successful load, -1 if directory does not exists, and -2 if file does not exist
+    public int LoadGame()
+    {
+        if(!Directory.Exists(folderPath))
+        {
+            Directory.CreateDirectory(folderPath);
+            return -1;
+        }
+
+        StringBuilder path = new StringBuilder(folderPath);
+        path.Append(fileName);
+
+        string filePath = path.ToString();
+
+        if(!File.Exists(filePath))
+        {
+            return -2;
+        }
+
+        StreamReader sr = File.OpenText(filePath);
+        sceneName = sr.ReadLine();
+        playerInventory.FromCSV(sr.ReadLine());
+        return 0;
+    }
+
+    public string[] GetSavedGames()
+    {
+        if(!Directory.Exists(folderPath))
+        {
+            Directory.CreateDirectory(folderPath);
+            return null;
+        }
+        return Directory.GetFiles(folderPath);
+    }
+
+    public int CountSavedGames()
+    {
+        if(!Directory.Exists(folderPath))
+        {
+            Directory.CreateDirectory(folderPath);
+            return 0;
+        }
+        return Directory.GetFiles(folderPath).Length;
+    }
+}
